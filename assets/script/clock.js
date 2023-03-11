584ae3fbac1695018b6ae361e6bfa6c1b1c26b45
Josh <josh@gable.tech>
2023-03-11 13:25:31 -0600

    base

---
+++ assets/script/clock.js
@@ -0,0 +1,59 @@
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+  Object-oriented JavaScript
+  Joshua Gable
+
+  Basic Clock
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+'use strict'
+
+let alarmTime = {};
+alarmTime.hour;
+alarmTime.minute;
+const setAlarm = document.querySelector('.setalarm');
+const userTime = document.querySelector('.timein')
+setInterval(updates, 1000);
+
+function updates() {
+  updateSeconds();
+  checkAlarm();
+}
+
+setAlarm.addEventListener('click', () => {
+  activateAlarm();
+});
+
+function activateAlarm() {
+  try {
+    if (userTime.value.substr(0, 2) <= 24 && userTime.value.substr(3, 2) <= 60) {
+      alarmTime.hour = userTime.value.substr(0, 2);
+      alarmTime.minute = userTime.value.substr(3, 2);
+      console.log(`Alarm set for: ${alarmTime.hour}:${alarmTime.minute}`);
+    }
+  } catch (err) {
+    throw ("Invalid Input");
+  }
+}
+
+function checkAlarm() {
+  let date = new Date();
+  if (date.getHours().toString().padStart(2, '0') === alarmTime.hour
+    && date.getMinutes().toString().padStart(2, '0') === alarmTime.minute) {
+    console.log('ring ring ring');
+  }
+}
+
+function updateSeconds() {
+  const timer = document.querySelector('.test');
+  const date = new Date();
+  const hour = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  let currentTime = hour + ":" + minutes + ':' + seconds;
+  timer.innerText = currentTime;
+}
+updateSeconds();
+
+
+
